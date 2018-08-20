cb3a15f279bc259d1d6a55706ff16941810c95c4
RayBDev <raybernard1337@gmail.com>
2018-08-20 12:25:04 -0600

    Added mobile menus

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import omnifoodLogoWhite from '../../assets/img/logo-white.png';
 import omnifoodLogoBlack from '../../assets/img/logo.png';
 import './Navigation.css';
 
-const Navigation = props => {
-    return (
-        <nav className={props.stickyNav ? 'sticky' : ''}>
-            <div className="row">
-                <img className="logo" src={omnifoodLogoWhite} alt="Omnifood Logo" />
-                <img className="logo-black" src={omnifoodLogoBlack} alt="Omnifood Logo" />
-                <ul className="main-nav">
-                    <li><a href="#features">Food Delivery</a></li>
-                    <li><a href="#steps">How it works</a></li>
-                    <li><a href="#cities">Our cities</a></li>
-                    <li><a href="#pricing">Sign up</a></li>
-                </ul>
-            </div>
-        </nav>
-    );
+class Navigation extends Component {
+    state = {
+        displayNav: false,
+        style: 'main-nav hidden',
+        icon: 'menu'
+    }
+
+    menuClickHandler = () => {
+        if (this.state.displayNav === false){
+            this.setState({
+                displayNav: true,
+                style: 'main-nav show',
+                icon: 'close'
+            });
+        } else {
+            this.setState({
+                displayNav: false,
+                style: 'main-nav hidden',
+                icon: 'menu'
+            });
+        }
+
+    }
+
+    render () {
+        return (
+            <nav className={this.props.stickyNav ? 'sticky' : ''}>
+                <div className="row">
+                    <img className="logo" src={omnifoodLogoWhite} alt="Omnifood Logo" />
+                    <img className="logo-black" src={omnifoodLogoBlack} alt="Omnifood Logo" />
+                    <ul className={this.state.style} >
+                        <li><a href="#features">Food Delivery</a></li>
+                        <li><a href="#steps">How it works</a></li>
+                        <li><a href="#cities">Our cities</a></li>
+                        <li><a href="#pricing">Sign up</a></li>
+                    </ul>
+                    <a className="mobile-nav-icon" onClick={this.menuClickHandler}><ion-icon name={this.state.icon}></ion-icon></a>
+                </div>
+            </nav>
+        );
+    }
 }
 
 export default Navigation;
